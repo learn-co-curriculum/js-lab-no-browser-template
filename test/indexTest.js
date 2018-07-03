81678867903e49c47557bcf3bda197b90e522271
maxwell <github email address>
2018-07-03 11:57:16 -0400

    updated template

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,16 @@
+
+
 describe("index.html", () => {
   it("exists", () => {
-    expect(document).toExist('index.html document not found')
+    expect(document.querySelector('body').innerHTML).to.include('hello')
+  })
+})
+
+describe("index.js", () => {
+  it("exists", () => {
+    expect(testVar).to.exist
+    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
+    expect(testFunc).to.exist
+    expect(testFunc()).to.equal('hi')
   })
 })
